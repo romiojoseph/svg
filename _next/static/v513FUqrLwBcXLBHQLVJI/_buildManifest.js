d9c35a95d8d2f04279d95c6b99b9bd53ecc71407
romiojoseph <55487671+romiojoseph@users.noreply.github.com>
2025-12-03 15:33:26 +0530

    Setup completed. Need to revisit later

---
+++ _next/static/v513FUqrLwBcXLBHQLVJI/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/svg//_next/:path+",
+        "destination": "/svg/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
